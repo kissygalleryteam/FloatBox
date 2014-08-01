dcadbe750fdeb6c051ab6835bff933576bb96d81
弘树 <tiehang.lth@alibaba-inc.com>
2014-08-01 13:18:46 +0800

    sync doc

---
+++ doc/meta/modules.js
@@ -0,0 +1,3 @@
+config({
+    'kg/FloatBox/index': {requires: ['node','base']}
+});
---
+++ doc/mini.js
@@ -0,0 +1,12 @@
+/**
+ * @fileoverview 
+ * @author 隐若<yinruo.nyj@taobao.com>
+ * @module FloatBox
+ **/
+KISSY.add(function(S, Component) {
+
+  return Component;
+
+}, {
+  requires: ["./index"]
+});
